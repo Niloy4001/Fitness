c1a5dd7a185449d45acd5be3358815ef56dcba98
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-21 00:13:07 +0600

    bmi calculator added

---
+++ js/bmi.js
@@ -0,0 +1,12 @@
+const calculateBtn = document.getElementById("calculate-btn");
+calculateBtn.addEventListener("click", function () {
+  const weight = parseFloat(document.getElementById("weight").value);
+  const heightValue = document.getElementById("height").value.split(".");
+  const heightFootValue = parseFloat(heightValue[0]);
+  const heightInchValue = parseFloat(heightValue[1]);
+  const heightFootValueInMeter = heightFootValue * 0.3048;
+  const heightInchValueInMeter = heightInchValue * 0.0254;
+  const height = heightFootValueInMeter + heightInchValueInMeter;
+  const BMI = weight / (height * height);
+  alert(`Your BMI is ${BMI.toFixed(2)}`);
+});
